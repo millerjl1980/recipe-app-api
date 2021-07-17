fbc6a20bbf6ca608f6c1c835595a7da96984cd4a
jmillerelements <jmiller@elements.org>
2021-07-17 11:37:47 -0400

    Test for email address required for new users

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves new user"""
+        if not email:
+            raise ValueError('User email address required')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
