3322ec9da5d7b16172cf686f5ae2d42170d4125c
jmillerelements <jmiller@elements.org>
2021-07-17 11:32:19 -0400

    Test for normalizing email address for new users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_nomalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@EMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'password')
+
+        self.assertEqual(user.email, email.lower())
