b2fbd2d71cc55ac1484e9f3cbfbeaa495b828fda
jmillerelements <jmiller@elements.org>
2021-07-17 11:45:35 -0400

    Added ability to create super users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,3 +26,12 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'password')
+
+    def test_create_new_superuser(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            'super@email.com', 'password'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
