ff2d36400250f20345c969a183cb356943ac0370
jmillerelements <jmiller@elements.org>
2021-07-25 14:53:39 -0400

    added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -59,3 +59,13 @@ class ModelTests(TestCase):
             name='Tomato',
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Burger',
+            time_minutes=5,
+            price=5.00,
+        )
+        self.assertEqual(str(recipe), recipe.title)
