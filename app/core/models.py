261032203c8379173b617ecc8fe470db2e99c6a9
jmillerelements <jmiller@elements.org>
2021-08-01 12:26:39 -0400

    added image field to recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,6 @@
+import uuid
+import os
+
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
@@ -5,6 +8,13 @@ from django.conf import settings
 
 
 # Create your models here.
+def recipe_image_file_path(instance, filename):
+    """Generate filepath for new recipe image"""
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+
+    return os.path.join('upload/recipe', filename)
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
@@ -77,6 +87,7 @@ class Recipe(models.Model):
     instructions = models.CharField(max_length=255, blank=True)
     notes = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -69,3 +71,12 @@ class ModelTests(TestCase):
             price=5.00,
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+        exp_path = f'upload/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
